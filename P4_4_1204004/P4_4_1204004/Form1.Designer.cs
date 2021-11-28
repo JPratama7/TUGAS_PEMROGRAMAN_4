08ad522b9a4fa9d18a8b0b03f6e9f00877db2f19
unknown <josepratama080@gmail.com>
2021-11-28 20:27:17 +0800

    upload

---
+++ P4_4_1204004/P4_4_1204004/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace P4_4_1204004
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbnum = new System.Windows.Forms.TextBox();
+            this.tbchar = new System.Windows.Forms.TextBox();
+            this.tbreq = new System.Windows.Forms.TextBox();
+            this.tbregex = new System.Windows.Forms.TextBox();
+            this.tbcom1 = new System.Windows.Forms.TextBox();
+            this.tbcom2 = new System.Windows.Forms.TextBox();
+            this.tbleng = new System.Windows.Forms.TextBox();
+            this.tbupper = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tblower = new System.Windows.Forms.TextBox();
+            this.epwarn = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epbenar = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epwarn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epbenar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbnum
+            // 
+            this.tbnum.Location = new System.Drawing.Point(231, 21);
+            this.tbnum.Name = "tbnum";
+            this.tbnum.Size = new System.Drawing.Size(100, 20);
+            this.tbnum.TabIndex = 0;
+            this.tbnum.Leave += new System.EventHandler(this.tbnum_Leave);
+            // 
+            // tbchar
+            // 
+            this.tbchar.Location = new System.Drawing.Point(231, 50);
+            this.tbchar.Name = "tbchar";
+            this.tbchar.Size = new System.Drawing.Size(100, 20);
+            this.tbchar.TabIndex = 1;
+            this.tbchar.Leave += new System.EventHandler(this.tbchar_Leave);
+            // 
+            // tbreq
+            // 
+            this.tbreq.Location = new System.Drawing.Point(231, 79);
+            this.tbreq.Name = "tbreq";
+            this.tbreq.Size = new System.Drawing.Size(100, 20);
+            this.tbreq.TabIndex = 2;
+            this.tbreq.Leave += new System.EventHandler(this.tbreq_Leave);
+            // 
+            // tbregex
+            // 
+            this.tbregex.Location = new System.Drawing.Point(231, 108);
+            this.tbregex.Name = "tbregex";
+            this.tbregex.Size = new System.Drawing.Size(100, 20);
+            this.tbregex.TabIndex = 3;
+            this.tbregex.Leave += new System.EventHandler(this.tbregex_Leave);
+            // 
+            // tbcom1
+            // 
+            this.tbcom1.Location = new System.Drawing.Point(231, 137);
+            this.tbcom1.Name = "tbcom1";
+            this.tbcom1.Size = new System.Drawing.Size(100, 20);
+            this.tbcom1.TabIndex = 4;
+            this.tbcom1.Leave += new System.EventHandler(this.tbcom1_Leave);
+            // 
+            // tbcom2
+            // 
+            this.tbcom2.Location = new System.Drawing.Point(361, 137);
+            this.tbcom2.Name = "tbcom2";
+            this.tbcom2.Size = new System.Drawing.Size(100, 20);
+            this.tbcom2.TabIndex = 5;
+            this.tbcom2.Leave += new System.EventHandler(this.tbcom2_Leave);
+            // 
+            // tbleng
+            // 
+            this.tbleng.Location = new System.Drawing.Point(231, 166);
+            this.tbleng.Name = "tbleng";
+            this.tbleng.Size = new System.Drawing.Size(100, 20);
+            this.tbleng.TabIndex = 6;
+            this.tbleng.Leave += new System.EventHandler(this.tbleng_Leave);
+            // 
+            // tbupper
+            // 
+            this.tbupper.Location = new System.Drawing.Point(231, 195);
+            this.tbupper.Name = "tbupper";
+            this.tbupper.Size = new System.Drawing.Size(100, 20);
+            this.tbupper.TabIndex = 7;
+            this.tbupper.Leave += new System.EventHandler(this.tbupper_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Numerik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Char";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Required";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Regex";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Comparison";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(97, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Length";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(97, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Upper Case";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(97, 224);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Lower Case";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "User Control";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tblower
+            // 
+            this.tblower.Location = new System.Drawing.Point(231, 224);
+            this.tblower.Name = "tblower";
+            this.tblower.Size = new System.Drawing.Size(100, 20);
+            this.tblower.TabIndex = 17;
+            this.tblower.Leave += new System.EventHandler(this.tblower_Leave);
+            // 
+            // epwarn
+            // 
+            this.epwarn.ContainerControl = this;
+            this.epwarn.Icon = ((System.Drawing.Icon)(resources.GetObject("epwarn.Icon")));
+            // 
+            // epbenar
+            // 
+            this.epbenar.ContainerControl = this;
+            this.epbenar.Icon = ((System.Drawing.Icon)(resources.GetObject("epbenar.Icon")));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 423);
+            this.Controls.Add(this.tblower);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbupper);
+            this.Controls.Add(this.tbleng);
+            this.Controls.Add(this.tbcom2);
+            this.Controls.Add(this.tbcom1);
+            this.Controls.Add(this.tbregex);
+            this.Controls.Add(this.tbreq);
+            this.Controls.Add(this.tbchar);
+            this.Controls.Add(this.tbnum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.epwarn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epbenar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbnum;
+        private System.Windows.Forms.TextBox tbchar;
+        private System.Windows.Forms.TextBox tbreq;
+        private System.Windows.Forms.TextBox tbregex;
+        private System.Windows.Forms.TextBox tbcom1;
+        private System.Windows.Forms.TextBox tbcom2;
+        private System.Windows.Forms.TextBox tbleng;
+        private System.Windows.Forms.TextBox tbupper;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tblower;
+        private System.Windows.Forms.ErrorProvider epwarn;
+        private System.Windows.Forms.ErrorProvider epbenar;
+    }
+}
+
---
+++ P4_4_1204004/P4_4_1204004/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace P4_4_1204004
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Numerik : " + tbnum.Text + "\nChar : " + tbchar.Text + "\nRequired: " + tbreq.Text + "\nRegex : " + tbregex.Text + "\nComparison : " + tbcom1.Text + " " + tbcom2.Text + "\nLength : " + tbleng.Text + "\nUpper : " + tbupper.Text + "\nLower : " + tblower.Text);
+
+        }
+
+        private void tbnum_Leave(object sender, EventArgs e)
+        {
+            int tbnumval;
+            if (int.TryParse(tbnum.Text, out tbnumval))
+            {
+                epbenar.SetError(tbnum, "OK");
+            }
+
+            else
+            {
+                epwarn.SetError(tbnum, "Silahkan Diisi");
+            }
+
+        }
+
+        private void tbchar_Leave(object sender, EventArgs e)
+        {
+            char tbcharval;
+            if (char.TryParse(tbchar.Text, out tbcharval))
+            {
+                epbenar.SetError(tbchar, "OK");
+            }
+
+            else
+            {
+                epwarn.SetError(tbchar, "Silahkan Diisi");
+            }
+        }
+
+        private void tbreq_Leave(object sender, EventArgs e)
+        {
+            if (tbchar.Text != "")
+            {
+                epbenar.SetError(tbreq, "OK");
+            }
+
+            else
+            {
+                epwarn.SetError(tbreq, "Silahkan Diisi");
+            }
+        }
+
+        private void tbregex_Leave(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(tbregex.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                epbenar.SetError(tbregex, "Betul!");
+            }
+            else
+            {
+                epwarn.SetError(tbregex, "Format salah !\nContoh: a@b.c");
+            }
+        }
+
+        private void tbcom1_Leave(object sender, EventArgs e)
+        {
+            if ((tbcom1.Text).All(Char.IsNumber))
+            {
+                if (tbcom1.Text == "")
+                {
+                    epwarn.SetError(tbcom1, "Angka 2 Belum terisi");
+                }
+
+                else
+                {
+                    epbenar.SetError(tbcom1, "Angka 2 Terisi");
+                }
+            }
+
+            else if (tbcom1.Text == "")
+            {
+                epwarn.SetError(tbcom1, "Textbox Huruf tidak boleh kosong");
+
+            }
+
+            else
+            {
+                epwarn.SetError(tbcom1, "inputan hanya boleh Angka!");
+            }
+        }
+
+        private void tbcom2_Leave(object sender, EventArgs e)
+        {
+            if ((tbcom2.Text).All(Char.IsNumber))
+            {
+                int Angka1;
+                int Angka2;
+                int.TryParse(tbcom1.Text, out Angka1);
+                int.TryParse(tbcom2.Text, out Angka2);
+                if (Angka1 > Angka2)
+                {
+                    epbenar.SetError(tbcom2, "Angka 1 lebih besar dari angka 2 ");
+                }
+
+                else
+                {
+                    epbenar.SetError(tbcom2, "Angka 2 lebih besar dari angka 1 ");
+
+                }
+            }
+
+            else if (tbcom2.Text == "")
+            {
+                epwarn.SetError(tbcom2, "Textbox Huruf tidak boleh kosong");
+
+            }
+
+            else
+            {
+                epwarn.SetError(tbcom2, "inputan hanya boleh Angka!");
+            }
+        }
+
+        private void tbleng_Leave(object sender, EventArgs e)
+        {
+            if (tbleng.Text != "")
+            {
+                int length = tbleng.Text.Length;
+                epbenar.SetError(tbleng, "Banyak Kata : " + length);
+            }
+            else
+            {
+                epwarn.SetError(tbleng, "Silahkan Diisi");
+            }
+        }
+
+        private void tbupper_Leave(object sender, EventArgs e)
+        {
+            if (tbupper.Text.All(char.IsUpper))
+            {
+                epbenar.SetError(tbupper, "OK");
+            }
+            else
+            {
+                epwarn.SetError(tbupper, "Bukan UPPERCASE");
+            }
+        }
+
+        private void tblower_Leave(object sender, EventArgs e)
+        {
+            if (tblower.Text.All(char.IsLower))
+            {
+                epbenar.SetError(tblower, "OK");
+            }
+            else
+            {
+                epwarn.SetError(tblower, "Bukan LOWERCASE");
+            }
+        }
+    }
+}
